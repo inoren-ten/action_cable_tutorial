b636764685e7df3e2bb215ef2b4ea0b208c995a8
HASURO <rentaro170928@icloud.com>
2023-06-24 20:16:18 +0900

    create action cable but for all user

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -11,6 +11,7 @@ class ChatsController < ApplicationController
     chat = Chat.new(user_id: @user.id, room_id: params[:room_id], body: params[:body])
     if chat.save
       render status: 200, json: chat
+      ActionCable.server.broadcast("chat", chat)
     else
       render status: 409, json: "save error"
     end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ActionCableTutorial
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_cable.mount_path = "/cable"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
